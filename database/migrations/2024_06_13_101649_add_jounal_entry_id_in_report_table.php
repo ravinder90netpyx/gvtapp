648a935761b78c516b51e8ee41128d35336a5158
sukhmeetraina@gmail.com <sukhmeetraina@gmail.com>
2024-06-13 09:55:09 +0530

    migratio to add journal entry id in report table

---
+++ database/migrations/2024_06_13_101649_add_jounal_entry_id_in_report_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      
+
+      Schema::table('report', function (Blueprint $table) {
+        $table->unsignedBigInteger('journal_entry_id')->nullable();
+    });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('report', function($table) {
+            $table->dropColumn('journal_entry_id');
+        });
+    }
+};
