3e6bfa20c19afa71ef3605206026e1cf9359a043
sukhmeetraina@gmail.com <sukhmeetraina@gmail.com>
2024-06-21 10:09:42 +0530

    migration of group table

---
+++ database/migrations/2024_06_13_000000_create_group_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('group', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name')->unique()->nullable();
+            $table->string('code')->unique()->nullable();
+
+            $table->enum('status', ['0', '1'])->default('1');
+            $table->enum('delstatus', ['0', '1'])->default('0');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('group');
+    }
+};
