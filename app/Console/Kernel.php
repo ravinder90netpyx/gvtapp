84b5e9797d209880fef746b927813c0bab2e581d
sukhmeetraina@gmail.com <sukhmeetraina@gmail.com>
2024-06-24 12:17:25 +0530

    cron changes every 3 hours

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -69,7 +69,7 @@ class Kernel extends ConsoleKernel{
                 // }
             }
         
-        })->everyMinute();/*->monthlyOn(22, '00:00');->everyFourHours()->days([0, 5, 6]);*/
+        })/*->everyMinute();/*->monthlyOn(22, '00:00');*/->everyThreeHours()->days([1, 2, 3]);
     }
 
     /**
