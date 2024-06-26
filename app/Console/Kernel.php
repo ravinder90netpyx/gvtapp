da469e042c79dd5a7b1a5dde39d997ce9aabc789
sukhmeetraina@gmail.com <sukhmeetraina@gmail.com>
2024-06-26 15:15:44 +0530

    testing cron

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -94,7 +94,7 @@ class Kernel extends ConsoleKernel{
                 // }
             }
         
-        })/*->everyMinute();/*->everyThreeHours()->days([1, 2, 3]);/*/->cron('12 15 1,8,12,14,18,26,30 * *');
+        })/*->everyMinute();/*->everyThreeHours()->days([1, 2, 3]);/*/->cron('18 15 1,8,12,14,18,26,30 * *');
         
             
         }
