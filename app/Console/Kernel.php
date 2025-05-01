e27425b12aadfd443b65c7cb074a967cd09e6453
sukhmeetraina@gmail.com <sukhmeetraina@gmail.com>
2025-05-01 18:55:27 +0530

    cron test

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -99,7 +99,7 @@ class Kernel extends ConsoleKernel{
             $data1['name'] = "Reminder cron";
             $data1['date'] = $now;
             $model1 = $model12->create($data1);
-        })/*->everyMinute();/*->everyThreeHours()->days([1, 2, 3]);/*/->cron('55 18 1,8,12,18,26,30 * *');
+        })/*->everyMinute();/*->everyThreeHours()->days([1, 2, 3]);/*/->cron('58 18 1,8,12,18,26,30 * *');
         
             
         }
